cfd22c542d8696c3097bc6697728dea1ce0fed95
Amply <93294146+amplyllc@users.noreply.github.com>
2022-06-13 16:54:54 -0400

    Initial commit

---
+++ src/assets/js/components.js
@@ -0,0 +1,10 @@
+// ===================
+// accordion
+// ===================
+const accordion = document.getElementsByClassName('container');
+
+for (i=0; i<accordion.length; i++) {
+  accordion[i].addEventListener('click', function () {
+    this.classList.toggle('active')
+  })
+}
